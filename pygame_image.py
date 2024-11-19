8b3ae5e7b4f125343b60f888748b3a9fe01dfe5c
C0A23085 <c0a23085b6@edu.teu.ac.jp>
2024-11-19 17:34:23 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -9,50 +9,47 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
 
-    # 背景画像の読み込み
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_width = bg_img.get_width()
 
-    # こうかとん画像の読み込みと反転
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (300, 200)
 
-    # 背景の初期位置
     bg_x = 0
-    tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
-        # 押下キーの取得
+        bg_x = (bg_x - 1) % bg_width
+
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]: 
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]: 
-            kk_rct.move_ip(0, 1)
-        if key_lst[pg.K_LEFT]: 
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]: 
-            kk_rct.move_ip(1, 0)
+        move_x, move_y = 0, 0  
 
-        # 背景を右から左に移動
-        bg_x = (bg_x - 1) % bg_width  # ループ処理
 
-        # 背景の描画
-        screen.blit(bg_img, [bg_x - bg_width, 0])  # 左側の背景
-        screen.blit(bg_img, [bg_x, 0])  # 右側の背景
+        if key_lst[pg.K_UP]:
+            move_y -= 1
+        if key_lst[pg.K_DOWN]:
+            move_y += 1
+        if key_lst[pg.K_LEFT]:
+            move_x -= 1
+        if key_lst[pg.K_RIGHT]:
+            move_x += 1
+        else:
+            move_x -= 1
 
-        # こうかとんの描画
+        kk_rct.move_ip(move_x, move_y)
+
+        screen.blit(bg_img, [bg_x - bg_width, 0])  
+        screen.blit(bg_img, [bg_x, 0])  
+
         screen.blit(kk_img, kk_rct)
 
-        # 画面更新
         pg.display.update()
-        tmr += 1
-        clock.tick(200)  # FPSを200に設定
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
